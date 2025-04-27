ced4aa3fa2b83591aac4d85fda260eef66b777b5
Guille Garcia <pasta0126@gmail.com>
2025-04-27 08:53:58 +0200

    nuget y demo

---
+++ src/OrangeParanoia.Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OrangeParanoia.Services;
+using OrangeParanoia.Services.Interfaces;
+
+var services = new ServiceCollection()
+    .AddSingleton<IArrayService, ArrayService>()
+    .AddSingleton<IAnswerService, AnswerService>()
+    .AddSingleton<ICardService, CardService>()
+    .AddSingleton<IDateService, DateService>()
+    .AddSingleton<INumberService, NumberService>()
+    .AddSingleton<IProgressionService, ProgressionService>()
+    .AddSingleton<IUtilityService, UtilityService>();
+
+var provider = services.BuildServiceProvider();
+
+var answerSvc = provider.GetRequiredService<IAnswerService>();
+
+Console.WriteLine("Magic 8-Ball: " + answerSvc.GetMagic8BallAnswer());
