2eacace98a3adfe3cbab73f798e051db726624a6
Guille Garcia <pasta0126@gmail.com>
2025-06-15 23:29:20 +0200

    wasm demo

--- src/OrangeParanoia.Wasm/Program.cs
+++ src/OrangeParanoia.Wasm/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using OrangeParanoia.Services;
+using OrangeParanoia.Services.Interfaces;
 using OrangeParanoia.Wasm;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +9,15 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<INumberService, NumberService>();
+builder.Services.AddScoped<IDateService, DateService>();
+builder.Services.AddScoped<IUtilityService, UtilityService>();
+builder.Services.AddScoped<IProgressionService, ProgressionService>();
+builder.Services.AddScoped<IArrayService, ArrayService>();
+builder.Services.AddScoped<IAnswerService, AnswerService>();
+builder.Services.AddScoped<ICardService, CardService>();
+builder.Services.AddScoped<IImageService, ImageService>();
+builder.Services.AddScoped<IStarWarsService, StarWarsService>();
 
 await builder.Build().RunAsync();
